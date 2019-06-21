d2f9832a8a3716019b93f1be045485ae036d8078
Vincent Långström <vincent.langstrom@gmail.com>
2019-06-21 14:26:48 +0300

    First test version of c++ code

--- lib/src/native/rpi_i2c_ext.cc
+++ lib/src/native/rpi_i2c_ext.cc
@@ -1,83 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <errno.h>
+#include <stdarg.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "include/dart_api.h"
 #include "include/dart_native_api.h"
 
+//TODO: maybe include errno and this
+/*
 Dart_Handle HandleError(Dart_Handle handle) {
   if (Dart_IsError(handle)) {
     Dart_PropagateError(handle);
   }
   return handle;
 }
-
-// === from wiringPiI2C.c ===
-
-// I2C definitions
-
-#define I2C_SLAVE       0x0703
-#define I2C_SMBUS       0x0720 /* SMBus-level access */
-
-#define I2C_SMBUS_READ  1
-#define I2C_SMBUS_WRITE 0
-
-// SMBus transaction types
-
-#define I2C_SMBUS_QUICK             0
-#define I2C_SMBUS_BYTE              1
-#define I2C_SMBUS_BYTE_DATA         2
-#define I2C_SMBUS_WORD_DATA         3
-#define I2C_SMBUS_PROC_CALL         4
-#define I2C_SMBUS_BLOCK_DATA        5
-#define I2C_SMBUS_I2C_BLOCK_BROKEN  6
-#define I2C_SMBUS_BLOCK_PROC_CALL   7  /* SMBus 2.0 */
-#define I2C_SMBUS_I2C_BLOCK_DATA    8
-
-// SMBus messages
-
-#define I2C_SMBUS_BLOCK_MAX     32 /* As specified in SMBus standard */
-#define I2C_SMBUS_I2C_BLOCK_MAX 32 /* Not specified but we use same structure */
-
-// Structures used in the ioctl() calls
-
-union i2c_smbus_data
-{
-  uint8_t  byte;
-  uint16_t word;
-  uint8_t  block[I2C_SMBUS_BLOCK_MAX + 2]; // block [0] is used for length + one more for PEC
-};
-
-struct i2c_smbus_ioctl_data
-{
-  char    read_write;
-  uint8_t command;
-  int     size;
-  union   i2c_smbus_data *data;
-};
-
-static inline int i2c_smbus_access(int fd, char rw, uint8_t command, int size, union i2c_smbus_data *data)
-{
-  struct i2c_smbus_ioctl_data args;
-
-  args.read_write = rw;
-  args.command    = command;
-  args.size       = size;
-  args.data       = data;
-  return ioctl(fd, I2C_SMBUS, &args);
-}
-
-// === end from wiringPiI2C.c ===
-
-// I2C Notes:
-// * https://github.com/rm-hull/wiringPi/blob/master/wiringPi/wiringPiI2C.c
-// * https://www.kernel.org/doc/Documentation/i2c/dev-interface
-// * https://www.kernel.org/doc/Documentation/i2c/smbus-protocol
 
 // The errno from the last I2C command
 static volatile int64_t lastErrno = 0;
@@ -90,198 +33,331 @@ void lastError(Dart_NativeArguments arguments) {
   Dart_SetIntegerReturnValue(arguments, lastErrno);
   Dart_ExitScope();
 }
+*/
 
-// Setup the I2C device at the given address and return the file id.
-// Negative return values indicate an error.
-//int _setupDevice(int address) native "setupDevice";
-void setupDevice(Dart_NativeArguments arguments) {
+/*
+ * serialOpen:
+ *	Open and initialise the serial port, setting all the right
+ *	port parameters - or as many as are required - hopefully!
+ *********************************************************************************
+ */
+
+//TODO: const char *device, const int baud
+int serialOpen(Dart_NativeArguments arguments)
+{
   Dart_EnterScope();
   Dart_Handle arg1 = HandleError(Dart_GetNativeArgument(arguments, 1));
+  Dart_Handle arg2 = HandleError(Dart_GetNativeArgument(arguments, 2));
 
-  int64_t address;
-  HandleError(Dart_IntegerToInt64(arg1, &address));
+  char device;
+  int baud;
+  HandleError(Dart_StringToCString(arg1, &device));
+  HandleError(Dart_IntegerToInt64(arg2, &baud));
 
-  //const char *device = "/dev/i2c-0"; // Raspberry Pi 1
-  const char *device = "/dev/i2c-1"; // Raspberry Pi 2 + 3
+  int64_t result;
+
+  struct termios options;
+  speed_t myBaud;
+  int status, fd;
+
+  switch (baud)
+  {
+    case      50:	myBaud =      B50 ; break ;
+    case      75:	myBaud =      B75 ; break ;
+    case     110:	myBaud =     B110 ; break ;
+    case     134:	myBaud =     B134 ; break ;
+    case     150:	myBaud =     B150 ; break ;
+    case     200:	myBaud =     B200 ; break ;
+    case     300:	myBaud =     B300 ; break ;
+    case     600:	myBaud =     B600 ; break ;
+    case    1200:	myBaud =    B1200 ; break ;
+    case    1800:	myBaud =    B1800 ; break ;
+    case    2400:	myBaud =    B2400 ; break ;
+    case    4800:	myBaud =    B4800 ; break ;
+    case    9600:	myBaud =    B9600 ; break ;
+    case   19200:	myBaud =   B19200 ; break ;
+    case   38400:	myBaud =   B38400 ; break ;
+    case   57600:	myBaud =   B57600 ; break ;
+    case  115200:	myBaud =  B115200 ; break ;
+    case  230400:	myBaud =  B230400 ; break ;
+    case  460800:	myBaud =  B460800 ; break ;
+    case  500000:	myBaud =  B500000 ; break ;
+    case  576000:	myBaud =  B576000 ; break ;
+    case  921600:	myBaud =  B921600 ; break ;
+    case 1000000:	myBaud = B1000000 ; break ;
+    case 1152000:	myBaud = B1152000 ; break ;
+    case 1500000:	myBaud = B1500000 ; break ;
+    case 2000000:	myBaud = B2000000 ; break ;
+    case 2500000:	myBaud = B2500000 ; break ;
+    case 3000000:	myBaud = B3000000 ; break ;
+    case 3500000:	myBaud = B3500000 ; break ;
+    case 4000000:	myBaud = B4000000 ; break ;
+
+    default:
+      //TODO: break out
+      Dart_SetIntegerReturnValue(arguments, -2);
+      Dart_ExitScope();
+  }
+
+  if ((fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK)) == -1)
+    //TODO: break out
+    Dart_SetIntegerReturnValue(arguments, -1);
+    Dart_ExitScope();
+
+  fcntl(fd, F_SETFL, O_RDWR);
+
+  // Get and modify current options:
+
+  tcgetattr(fd, &options);
+
+  cfmakeraw(&options);
+  cfsetispeed(&options, myBaud);
+  cfsetospeed(&options, myBaud);
+
+  options.c_cflag |= (CLOCAL | CREAD);
+  options.c_cflag &= ~PARENB;
+  options.c_cflag &= ~CSTOPB;
+  options.c_cflag &= ~CSIZE;
+  options.c_cflag |= CS8;
+  options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+  options.c_oflag &= ~OPOST;
+
+  options.c_cc[VMIN] = 0;
+  options.c_cc[VTIME] = 100; // Ten seconds (100 deciseconds)
+
+  tcsetattr(fd, TCSANOW, &options);
+
+  ioctl(fd, TIOCMGET, &status);
+
+  status |= TIOCM_DTR;
+  status |= TIOCM_RTS;
+
+  ioctl(fd, TIOCMSET, &status);
+
+  usleep(10000); // 10mS
+
+  Dart_SetIntegerReturnValue(arguments, fd);
+  Dart_ExitScope();
+}
+
+/*
+ * serialFlush:
+ *	Flush the serial buffers (both tx & rx)
+ *********************************************************************************
+ */
+
+void serialFlush(Dart_NativeArguments arguments)
+{
+  Dart_EnterScope();
+  Dart_Handle arg1 = HandleError(Dart_GetNativeArgument(arguments, 1));
 
   int fd;
-  int64_t result;
+  HandleError(Dart_IntegerToInt64(arg1, &fd));
 
-  if ((fd = open(device, O_RDWR)) < 0) {
-    result = -1;
-    lastErrno = errno;
-  } else if (ioctl(fd, I2C_SLAVE, address) < 0) {
-    close(fd);
-    result = -2;
-    lastErrno = errno;
-  } else {
-    result = fd;
-    lastErrno = 0;
-  }
+  tcflush(fd, TCIOFLUSH);
 
-  Dart_SetIntegerReturnValue(arguments, result);
+  //return 1 for now TODO: don't return anything?
+  Dart_SetIntegerReturnValue(arguments, 1);
   Dart_ExitScope();
 }
 
-// Dispose of the I2C device and return 0 to indicate success.
-// Negative return values indicate an error.
-//int _disposeDevice(int fd) native "disposeDevice";
-void disposeDevice(Dart_NativeArguments arguments) {
+/*
+ * serialClose:
+ *	Release the serial port
+ *********************************************************************************
+ */
+
+void serialClose(Dart_NativeArguments arguments)
+{
   Dart_EnterScope();
   Dart_Handle arg1 = HandleError(Dart_GetNativeArgument(arguments, 1));
 
-  int64_t fd;
+  int fd;
   HandleError(Dart_IntegerToInt64(arg1, &fd));
 
-  int64_t result;
-  if (close(fd) < 0) {
-    result = -1;
-    lastErrno = errno;
-  } else {
-    result = 0;
-    lastErrno = 0;
-  }
+  close(fd);
 
-  Dart_SetIntegerReturnValue(arguments, result);
+  //return 1 for now TODO: don't return anything?
+  Dart_SetIntegerReturnValue(arguments, 1);
   Dart_ExitScope();
 }
 
-// Read an 8 bit value from a register on the device.
-// Negative return values indicate an error.
-//int _readByte(int fd, int register) native "readByte";
-void readByte(Dart_NativeArguments arguments) {
+/*
+ * serialPutchar:
+ *	Send a single character to the serial port
+ *********************************************************************************
+ */
+
+//TODO: (const int fd, const unsigned char c)
+void serialPutchar(Dart_NativeArguments arguments)
+{
   Dart_EnterScope();
   Dart_Handle arg1 = HandleError(Dart_GetNativeArgument(arguments, 1));
   Dart_Handle arg2 = HandleError(Dart_GetNativeArgument(arguments, 2));
 
-  int64_t fd;
-  int64_t reg;
+  int fd;
+  unsigned char c;
   HandleError(Dart_IntegerToInt64(arg1, &fd));
-  HandleError(Dart_IntegerToInt64(arg2, &reg));
+  //TODO: find the right dart/c conversion method
+  HandleError(Dart_StringToCString(arg2, &c));
+  //set to x for now
+  c = "x";
 
-  int64_t result = 0;
-  union i2c_smbus_data data;
-  if (i2c_smbus_access(fd, I2C_SMBUS_READ, reg, I2C_SMBUS_BYTE_DATA, &data)) {
-    result = -1;
-    lastErrno = errno;
-  } else {
-    result = data.byte & 0xFF;
-    lastErrno = 0;
-  }
+  write(fd, &c, 1);
 
-  Dart_SetIntegerReturnValue(arguments, result);
+  //return 1 for now TODO: don't return anything?
+  Dart_SetIntegerReturnValue(arguments, 1);
   Dart_ExitScope();
 }
 
-// Read 1 to 32 bytes from the device (not a register on the device).
-// Return the number of bytes read.
-// Negative return values indicate an error.
-//int _readBytes(int fd, List<int> values) native "readBytes";
-void readBytes(Dart_NativeArguments arguments) {
-  Dart_EnterScope();
-  Dart_Handle arg1 = HandleError(Dart_GetNativeArgument(arguments, 1));
-  Dart_Handle dartList = HandleError(Dart_GetNativeArgument(arguments, 2));
+/*
+ * serialPuts:
+ *	Send a string to the serial port
+ *********************************************************************************
+ */
 
-  int64_t fd;
-  HandleError(Dart_IntegerToInt64(arg1, &fd));
-
-  int length;
-  HandleError(Dart_ListLength(dartList, &length));
-
-  char buf[34];
-  int64_t result = read(fd, buf, length);
-  for (int index = 0; index < length; ++index) {
-    HandleError(Dart_ListSetAt(
-      dartList, index, HandleError(Dart_NewInteger(buf[index]))));
-  }
-
-  Dart_SetIntegerReturnValue(arguments, result);
-  Dart_ExitScope();
-}
-
-// Write a byte to the device.
-// Negative return values indicate an error.
-//int _writeByte(int fd, int register, int data) native "writeByte";
-void writeByte(Dart_NativeArguments arguments) {
+//TODO: (const int fd, const char *s)
+void serialPuts(Dart_NativeArguments arguments)
+{
   Dart_EnterScope();
   Dart_Handle arg1 = HandleError(Dart_GetNativeArgument(arguments, 1));
   Dart_Handle arg2 = HandleError(Dart_GetNativeArgument(arguments, 2));
-  Dart_Handle arg3 = HandleError(Dart_GetNativeArgument(arguments, 3));
 
-  int64_t fd;
-  int64_t reg;
-  int64_t value;
+  int fd;
+  char *s;
   HandleError(Dart_IntegerToInt64(arg1, &fd));
-  HandleError(Dart_IntegerToInt64(arg2, &reg));
-  HandleError(Dart_IntegerToInt64(arg3, &value));
+  //TODO: check that this is the right dart/c conversion method
+  HandleError(Dart_StringToCString(arg2, &s));
+
+  write(fd, s, strlen(s));
+
+  //return 1 for now TODO: don't return anything?
+  Dart_SetIntegerReturnValue(arguments, 1);
+  Dart_ExitScope();
+}
+
+//TODO: implement?
+/*
+ * serialPrintf:
+ *	Printf over Serial
+ *********************************************************************************
+ */
+
+/*
+ * serialDataAvail:
+ *	Return the number of bytes of data avalable to be read in the serial port
+ *********************************************************************************
+ */
+
+int serialDataAvail(Dart_NativeArguments arguments)
+{
+  Dart_EnterScope();
+  Dart_Handle arg1 = HandleError(Dart_GetNativeArgument(arguments, 1));
+
+  int fd;
+  HandleError(Dart_IntegerToInt64(arg1, &fd));
+
+  //TODO: make it int64_t?
+  int result;
+
+  if (ioctl(fd, FIONREAD, &result) == -1)
+    result = -1;
+
+  //check that this return works
+  Dart_SetIntegerReturnValue(arguments, result);
+  Dart_ExitScope();
+}
+
+/*
+ * serialGetchar:
+ *	Get a single character from the serial device.
+ *	Note: Zero is a valid character and this function will time-out after
+ *	10 seconds.
+ *********************************************************************************
+ */
+
+int serialGetchar(Dart_NativeArguments arguments)
+{
+  Dart_EnterScope();
+  Dart_Handle arg1 = HandleError(Dart_GetNativeArgument(arguments, 1));
+
+  int fd;
+  HandleError(Dart_IntegerToInt64(arg1, &fd));
 
   int64_t result = 0;
-  union i2c_smbus_data data;
-  data.byte = value & 0xFF;
-  if (i2c_smbus_access(fd, I2C_SMBUS_WRITE, reg, I2C_SMBUS_BYTE_DATA, &data)) {
-    result = -1;
-    lastErrno = errno;
-  } else {
-    lastErrno = 0;
-  }
 
+  uint8_t x;
+
+  if (read(fd, &x, 1) != 1)
+    result = -1;
+
+  result = ((int)x) & 0xFF;
+
+  //check that this return works
   Dart_SetIntegerReturnValue(arguments, result);
   Dart_ExitScope();
 }
 
 // ===== Infrastructure methods ===============================================
 
-struct FunctionLookup {
-  const char* name;
+struct FunctionLookup
+{
+  const char *name;
   Dart_NativeFunction function;
 };
 
 FunctionLookup function_list[] = {
-  {"disposeDevice", disposeDevice},
-  {"lastError", lastError},
-  {"readByte", readByte},
-  {"readBytes", readBytes},
-  {"setupDevice", setupDevice},
-  {"writeByte", writeByte},
-  {NULL, NULL}
-};
+    {"disposeDevice", disposeDevice},
+    {"lastError", lastError},
+    {"readByte", readByte},
+    {"readBytes", readBytes},
+    {"setupDevice", setupDevice},
+    {"writeByte", writeByte},
+    {NULL, NULL}};
 
 FunctionLookup no_scope_function_list[] = {
-  {NULL, NULL}
-};
+    {NULL, NULL}};
 
 // Resolve the Dart name of the native function into a C function pointer.
 // This is called once per native method.
 Dart_NativeFunction ResolveName(Dart_Handle name,
                                 int argc,
-                                bool* auto_setup_scope) {
-  if (!Dart_IsString(name)) {
+                                bool *auto_setup_scope)
+{
+  if (!Dart_IsString(name))
+  {
     return NULL;
   }
   Dart_NativeFunction result = NULL;
-  if (auto_setup_scope == NULL) {
+  if (auto_setup_scope == NULL)
+  {
     return NULL;
   }
 
   Dart_EnterScope();
-  const char* cname;
+  const char *cname;
   HandleError(Dart_StringToCString(name, &cname));
 
-  for (int i=0; function_list[i].name != NULL; ++i) {
-    if (strcmp(function_list[i].name, cname) == 0) {
+  for (int i = 0; function_list[i].name != NULL; ++i)
+  {
+    if (strcmp(function_list[i].name, cname) == 0)
+    {
       *auto_setup_scope = true;
       result = function_list[i].function;
       break;
     }
   }
 
-  if (result != NULL) {
+  if (result != NULL)
+  {
     Dart_ExitScope();
     return result;
   }
 
-  for (int i=0; no_scope_function_list[i].name != NULL; ++i) {
-    if (strcmp(no_scope_function_list[i].name, cname) == 0) {
+  for (int i = 0; no_scope_function_list[i].name != NULL; ++i)
+  {
+    if (strcmp(no_scope_function_list[i].name, cname) == 0)
+    {
       *auto_setup_scope = false;
       result = no_scope_function_list[i].function;
       break;
@@ -294,13 +370,16 @@ Dart_NativeFunction ResolveName(Dart_Handle name,
 
 // Initialize the native library.
 // This is called once when the native library is loaded.
-DART_EXPORT Dart_Handle rpi_i2c_ext_Init(Dart_Handle parent_library) {
-  if (Dart_IsError(parent_library)) {
+DART_EXPORT Dart_Handle rpi_i2c_ext_Init(Dart_Handle parent_library)
+{
+  if (Dart_IsError(parent_library))
+  {
     return parent_library;
   }
   Dart_Handle result_code =
       Dart_SetNativeResolver(parent_library, ResolveName, NULL);
-  if (Dart_IsError(result_code)) {
+  if (Dart_IsError(result_code))
+  {
     return result_code;
   }
   return Dart_Null();
