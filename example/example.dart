1fb9f9f44f0a4d81c6299b63823ccba668585a48
Vincent Långström <vincent.langstrom@gmail.com>
2019-06-29 12:16:29 +0300

    Working example

--- example/example.dart
+++ example/example.dart
@@ -1,5 +1,6 @@
+import 'dart:math';
+import 'dart:async';
 import 'package:rpi_serial/rpi_serial.dart';
-
 import 'basic_device.dart';
 
 main() async {
@@ -8,11 +9,12 @@ main() async {
   serial_connection.dispose();
 }
 
-testSensor(BasicSerialDevice arduino) {
+testSensor(BasicSerialDevice device) async {
   print(
       "Entering endless loop mode. Please connect a cable between the rx and tx pins.");
   while (true) {
-    //TODO: add input here, then send that
-    print(arduino.readChar());
+    device.writeChar(Random().nextInt(10).toString());
+    print(device.readChar());
+    await Future.delayed(Duration(milliseconds: 500));
   }
 }
